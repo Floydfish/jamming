e8cb5bc25354ae4d1bbf14f7d24db872d528e6cb
Floydfish <38674510+Floydfish@users.noreply.github.com>
2020-05-26 11:06:49 +0200

    change to ES6 APP.js and stuck at tracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,19 +5,23 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+export class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { searchResults: [name, artist, album, id] }
+  }
+  render() {
+    return(
+      <div>
+        <h1>Ja<span className="highlight">mm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
-
-export default App;
